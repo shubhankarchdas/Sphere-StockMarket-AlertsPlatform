67610c228ef14be9bd3134dd63af954da9a6d07a
shubhankarchdas <shubhankarchdas@gmail.com>
2025-09-27 11:54:03 +0530

    zoom clone by next.js and ts /setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistsans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistMono.className} bg-dark-2`}>
         {children}
       </body>
     </html>
